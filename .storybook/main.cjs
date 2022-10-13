b2926d76ecd87626482ab0e2f0cea4a5006088ce
Giovani-O <giovani.o@outlook.com.br>
2022-10-13 19:57:51 -0300

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFInal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/lab-ds/'
+    }
+
+    return config
   }
 }
